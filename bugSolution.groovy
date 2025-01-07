e630d7685a305e66b1eabc86269da939f4d9eef6
BugHunter-X <askzenai@gmail.com>
2025-01-07 17:40:43 +0100

    Add bugSolution.groovy file

---
+++ bugSolution.groovy
@@ -0,0 +1,13 @@
+```groovy
+def myMethod(List<String> list) {
+  if (list != null) {
+    list.eachWithIndex { String item, int index ->
+      println "Item $index: $item"
+    }
+  } else {
+    println "The list is null"
+  }
+}
+
+myMethod(null)
+```
